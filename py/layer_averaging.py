5aa4666fc4894089bbb9b05c43e4ca5ee78f355e
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2020-08-30 22:44:25 +0900

    Added

--- py/layer_averaging.py
+++ py/layer_averaging.py
@@ -1,6 +1,6 @@
-# ensemble_averaging.py
+# layer_averaging.py
 #   Tomomasa Uchida
-#   2019/05/25
+#   2020/08/29
 
 import numpy as np
 import cv2
@@ -9,8 +9,8 @@ import cv2
 import sys
 args = sys.argv
 if len(args) != 4:
-    print("\nUSAGE   : $ python ensemble_averaging.py [input_images_path] [repeat_level] [image_resolution]")
-    print("EXAMPLE : $ python ensemble_averaging.py ../OUTPUT/LR100/IMAGE_DATA 100 1000\n")
+    print("\nUSAGE   : $ python layer_averaging.py [input_images_path] [num_of_layers] [image_resolution]")
+    print("EXAMPLE : $ python layer_averaging.py ../IMAGE_DATA 10 512\n")
     sys.exit()
 
 
@@ -26,23 +26,23 @@ def ReadImage( _img_name ):
 
 
 
-def run( _repeat_level, _image_resol, _serial_img_path ):
+def run( _num_of_layers, _image_resol, _serial_img_path ):
     # Prepare empty numpy array
-    R_pixel_values = np.empty( (_image_resol*1, _image_resol*1, _repeat_level), np.uint8 )
-    G_pixel_values = np.empty( (_image_resol*1, _image_resol*1, _repeat_level), np.uint8 )
-    B_pixel_values = np.empty( (_image_resol*1, _image_resol*1, _repeat_level), np.uint8 )
+    R_pixel_values = np.empty( (_image_resol*1, _image_resol*1, _num_of_layers), np.uint8 )
+    G_pixel_values = np.empty( (_image_resol*1, _image_resol*1, _num_of_layers), np.uint8 )
+    B_pixel_values = np.empty( (_image_resol*1, _image_resol*1, _num_of_layers), np.uint8 )
 
     # Read intermediate images
-    for i in range( _repeat_level ):
+    for i in range( _num_of_layers ):
         # Read each ensemble image
-        tmp_image_RGB = ReadImage( _serial_img_path + "ensemble"+str(i+1)+".bmp" )
+        tmp_image_RGB = ReadImage( _serial_img_path + "Layer"+str(i+1)+".bmp" )
 
         # Split into RGB and add to numpy array
         R_pixel_values[:,:,i] = tmp_image_RGB[:,:,0] # R
         G_pixel_values[:,:,i] = tmp_image_RGB[:,:,1] # G
         B_pixel_values[:,:,i] = tmp_image_RGB[:,:,2] # B
 
-        if i == _repeat_level-1:
+        if i == _num_of_layers-1:
             print("R :", R_pixel_values.shape)
             print("G :", G_pixel_values.shape)
             print("B :", B_pixel_values.shape)
@@ -61,21 +61,21 @@ def run( _repeat_level, _image_resol, _serial_img_path ):
     # (0,    1,    2) → (   1,    2, 0)
     RGB_mean = np.array([R_mean, G_mean, B_mean]).transpose((1, 2, 0))
     
-    # Save result image
+    # Save the result image
     RGB_mean_BGR = cv2.cvtColor(RGB_mean, cv2.COLOR_RGB2BGR)
-    cv2.imwrite("OUTPUT_DATA/ensemble_averaging.png", RGB_mean_BGR)
+    cv2.imwrite("OUTPUT_DATA/Layer_Averaging.png", RGB_mean_BGR)
 
 
 
 if __name__ == "__main__":
-    # Set repeat level
-    repeat_level = int(args[2])
-    print("Repeat Level     :", repeat_level)
+    # Set the number of layers
+    num_of_layers = int(args[2])
+    print("Number of Layers :", num_of_layers)
 
     # Set image resolution
     image_resol = int(args[3])
     print("Image Resolution :", image_resol)
 
-    # Read intermediate images
+    # Read target images
     serial_img_path = args[1] + "/"
-    run( repeat_level, image_resol, serial_img_path )
+    run( num_of_layers, image_resol, serial_img_path )
